c9e10a2b427e2768283dfb9fd0f24587b5430f49
THOTA ABHI RAMA KRISHNA PRASAD <96060889+Arkp-1289@users.noreply.github.com>
2026-04-26 18:50:18 +0530

    Add: sqrtx - submission-2

---
+++ Data Structures & Algorithms/sqrtx/submission-2.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int mySqrt(int x) {
+
+        long l=1,r=x;
+
+        while (l<=r){
+            long mid = (l+r)/2;
+            // System.out.println(mid);
+            if (mid*mid==x){
+                return (int) mid;
+            }
+            if (mid*mid>x){
+                r=mid-1;
+            } else{
+                l=mid+1;
+            }
+        }
+        return (int) l-1;
+        
+    }
+}
