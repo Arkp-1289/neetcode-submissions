e39c6967a8706d118edfe47d5898eb298ec39146
THOTA ABHI RAMA KRISHNA PRASAD <96060889+Arkp-1289@users.noreply.github.com>
2026-05-04 22:33:31 +0530

    Add: search-in-rotated-sorted-array-ii - submission-1

---
+++ Data Structures & Algorithms/search-in-rotated-sorted-array-ii/submission-1.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean search(int[] nums, int target) {
+        int n=nums.length;
+
+        int l=0,r=n-1;
+        
+        while (l<=r){
+            int mid=(l+r)/2;
+
+
+            if (nums[mid]==target){return true;}
+
+            if (nums[mid]>nums[l]){
+                if (nums[l]<=target && nums[mid]>=target){
+                    r=mid-1;
+                } else{
+                    l=mid+1;
+                }
+            } else if (nums[l]>nums[mid]){
+                if (nums[mid]<=target && nums[r]>=target){
+                    l=mid+1;
+                } else{
+                    r=mid-1;
+                }
+            } else{l++;}
+        }
+        return false;
+        
+    }
+}
